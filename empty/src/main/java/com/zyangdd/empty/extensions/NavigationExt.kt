a63ea648ff5cd69e3e43e7eede249f933b0e18b1
giangdd <giang.dd@review-ty.com>
2021-01-26 01:15:36 +0700

    feat: update base

---
+++ empty/src/main/java/com/zyangdd/empty/extensions/NavigationExt.kt
@@ -0,0 +1,16 @@
+package com.zyangdd.empty.extensions
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+
+fun Activity.openPlayStore() {
+    val action = Intent.ACTION_VIEW
+    try {
+        val uri = Uri.parse("market://details?id=$packageName")
+        startActivity(Intent(action, uri))
+    } catch (e: android.content.ActivityNotFoundException) {
+        val uri = Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+        startActivity(Intent(action, uri))
+    }
+}
