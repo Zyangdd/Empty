ad6fd06eb26f57ae463fd73cc5494bd45b28edb9
giangdd <giang.dd@review-ty.com>
2021-01-26 01:25:29 +0700

    feat: upgrade lib version

--- app/src/main/java/com/zyangdd/empty/MainActivity.kt
+++ app/src/main/java/com/zyangdd/empty/MainActivity.kt
@@ -1,32 +1,28 @@
 package com.zyangdd.empty
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import com.zyangdd.empty.base.AppDialog
+import com.zyangdd.empty.databinding.ActivityMainBinding
 import com.zyangdd.empty.widget.MultiStateView.State
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val viewModel by viewModels<ViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button.setOnClickListener {
-            AppDialog(this).apply {
-                title = "Alert"
-//                negative = "negative"
-                positive = "positive"
-//                neutral = "neutral"
-                message = "Have an new app version! Do you want to upgrade now?"
-            }.show()
+        binding.button.setOnClickListener {
+//            AppDialog(this).apply {
+//                title = "Alert"
+////                negative = "negative"
+//                positive = "positive"
+////                neutral = "neutral"
+//                message = "Have an new app version! Do you want to upgrade now?"
+//            }.show()
         }
-        button2.setOnClickListener { state_view.update(State.LOADING) }
-        button3.setOnClickListener { state_view.update(State.EMPTY) }
-        button4.setOnClickListener { state_view.update(State.ERROR) }
+        binding.button2.setOnClickListener { binding.stateView.update(State.LOADING) }
+        binding.button3.setOnClickListener { binding.stateView.update(State.EMPTY) }
+        binding.button4.setOnClickListener { binding.stateView.update(State.ERROR) }
     }
 }
